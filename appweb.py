e46485b402ca4e592f3c4a5cd1f592b096785233
tretap <email_id>
2017-11-30 10:04:11 +0700

    Add member with csv function & Delete bin file

--- appweb.py
+++ appweb.py
@@ -160,6 +160,7 @@ def add_member_class_web():
     for line in f:
         information_line = line.split(",")
         if information_line[0] == "" or information_line[1] == "":
+            os.remove(destination)
             return load_memberClass_web()
 
         if  check_account_db(information_line[0]):
@@ -169,6 +170,7 @@ def add_member_class_web():
             _id = add_account(information_line[0],"password",(information_line[1].split(" "))[0],(information_line[1].split(" "))[1],information_line[0],"student")
             add_member(session.get('class'),int(_id))
 
+    os.remove(destination)
 
     return load_memberClass_web()
 
